8d45c89c9af52e9518514b5cec671685f5717b7b
Dawid Wydra <72543889+ejsiik@users.noreply.github.com>
2023-03-15 08:15:51 +0100

    init commit

---
+++ CameraApp/CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by user on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
